dd9b6c23736478461a677f21ac1efbe9dede4446
leederacola <rmgforce@gmail.com>
2017-05-26 15:56:49 -0400

    initial commit.  Home contoller, BoardGame Model and Index view created

---
+++ BoardGameBuddy/Models/BoardGame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoardGameBuddy.Models
+{
+    public class BoardGame
+    {
+        public string Title { get; set; }
+        public string Id { get; set; }
+        public string PlayerCount { get; set; }
+        public int BestPlayerCount { get; set; }
+        public int PlayTimeMax { get; set; }
+        public int MaRating { get; set; }
+        public int GeekRating { get; set; }
+    }
+}
