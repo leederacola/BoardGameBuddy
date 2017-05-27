5182ce032d4a3fcf3326ef28a0d355de18d67cce
leederacola <rmgforce@gmail.com>
2017-05-27 11:55:50 -0400

    updated comic book repository

--- BoardGameBuddy/Data/BoardGameRepository.cs
+++ BoardGameBuddy/Data/BoardGameRepository.cs
@@ -10,10 +10,92 @@ namespace BoardGameBuddy.Data
 {
     public class BoardGameRepository
     {
-        public BoardGame GetBoardGame()
+        
+        private static BoardGame[] _boardGames = new BoardGame[]
         {
-            return null;
+            new BoardGame()
+            {
+                Id = 1,
+                Title = "Blood Rage",
+                PlayerCountMin = 2,
+                PlayerCountMax = 5,
+                BestPlayerCount = 4,
+                PlayTimeMax = 90,
+                MyRating = 7.5,
+                GeekRating = 8.2,
+            },
+            new BoardGame()
+            {
+                Id = 2,
+                Title = "Lost Citiese",
+                PlayerCountMin = 2,
+                PlayerCountMax = 2,
+                BestPlayerCount = 2,
+                PlayTimeMax = 30,
+                MyRating = 8.1,
+                GeekRating = 7.1,
+            },
+            new BoardGame()
+            {
+                Id = 3,
+                Title = "Dead of Winter: A Crossroads Game ",
+                PlayerCountMin = 2,
+                PlayerCountMax = 5,
+                BestPlayerCount = 4,
+                PlayTimeMax = 210,
+                MyRating = 7.5,
+                GeekRating = 7.9,
+            },
+            new BoardGame()
+            {
+                Id = 4,
+                Title = "Cosmic Encounter",
+                PlayerCountMin = 3,
+                PlayerCountMax = 6,
+                BestPlayerCount = 5,
+                PlayTimeMax = 120,
+                MyRating = 9.0,
+                GeekRating = 7.6,
+            },
+            new BoardGame()
+            {
+                Id = 5,
+                Title = "Codenames",
+                PlayerCountMin = 2,
+                PlayerCountMax = 8,
+                BestPlayerCount = 6,
+                PlayTimeMax = 15,
+                MyRating = 8,
+                GeekRating = 7.9,
+            }
+        };  //end array
+
+        public BoardGame[] GetBoardGames()
+        {
+            return _boardGames;
         }
-    
+
+
+        //-----------------------------------------------------------------------
+
+        public BoardGame GetBoardGame(int id)
+        //!
+        //!
+        {
+            BoardGame boardGameToReturn = null;
+
+            foreach (var boardGame in _boardGames)
+            {
+                if (boardGame.Id == id)
+                {
+                    boardGameToReturn = boardGame;
+                    break;
+                }
+            }
+
+            return boardGameToReturn;
+        }
+
+
     }
 }
--- BoardGameBuddy/Models/BoardGame.cs
+++ BoardGameBuddy/Models/BoardGame.cs
@@ -7,12 +7,13 @@ namespace BoardGameBuddy.Models
 {
     public class BoardGame
     {
+        public int Id { get; set; }
         public string Title { get; set; }
-        public string Id { get; set; }
-        public string PlayerCount { get; set; }
+        public int PlayerCountMin { get; set; }
+        public int PlayerCountMax { get; set; }
         public int BestPlayerCount { get; set; }
-        public int PlayTimeMax { get; set; }
-        public int MaRating { get; set; }
-        public int GeekRating { get; set; }
+        public int PlayTimeMax { get; set; }    //mins
+        public double MyRating { get; set; }
+        public double GeekRating { get; set; }
     }
 }
