312312459d3e6006e5d3d1ea4c384332377a1775
leederacola <rmgforce@gmail.com>
2017-05-27 11:10:46 -0400

    Added GameLibiary view,  updated homerController with ActionLinker, created BoardGame repository.  Added (ComicBookTut) for quick reffrence to treehouse code.

--- BoardGameBuddy/Controllers/HomeController.cs
+++ BoardGameBuddy/Controllers/HomeController.cs
@@ -4,15 +4,25 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
+
 namespace BoardGameBuddy.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
+        //adds date time info in viewbag.greating......passes it to view
+        //returns view() by deafult index (name of method)
         public ActionResult Index()
         {
             int hour = DateTime.Now.Hour;
             ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+
+        //returns view from html.ActionLinker in index html
+         public ViewResult GameLibrary()
+        {
             return View();
         }
     }
---
+++ BoardGameBuddy/Data/BoardGameRepository.cs
@@ -0,0 +1,19 @@
+﻿using BoardGameBuddy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+//this BoardGameRepository will hold BoardGame data
+
+namespace BoardGameBuddy.Data
+{
+    public class BoardGameRepository
+    {
+        public BoardGame GetBoardGame()
+        {
+            return null;
+        }
+    
+    }
+}
